34d9e3b92344a75544858a03039fb77dd3f7f0b5
Adrian4401 <adrianzakrzewski4401@gmail.com>
2024-11-07 21:17:42 +0100

    Location with openStreetView added.

--- src/app/annoucement-details/annoucement-details-info/annoucement-details-location/annoucement-details-location.component.ts
+++ src/app/annoucement-details/annoucement-details-info/annoucement-details-location/annoucement-details-location.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'annoucement-details-location',
@@ -10,6 +11,30 @@ export class AnnoucementDetailsLocationComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  private map: L.Map | undefined;
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+
+  private initMap(): void {
+    this.map = L.map('map').setView([52.652499, 19.057948], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; OpenStreetMap contributors',
+    }).addTo(this.map);
+
+    const circle = L.circle([52.652499, 19.057948], {
+      color: '#4c46c8',
+      fillColor: '#4c46c8',
+      fillOpacity: 0.3,
+      radius: 500
+    });
+
+    circle.addTo(this.map);
   }
 
 }
